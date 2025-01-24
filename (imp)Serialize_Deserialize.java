1532d286ccb0b61a738356d34ec594f9409c239b
Sagnik Ghosal <122097465+sagnikghosal007@users.noreply.github.com>
2025-01-24 14:34:36 +0530

    Create (imp)Serialize_Deserialize.java

---
+++ (imp)Serialize_Deserialize.java
@@ -0,0 +1,45 @@
+//TIME COMPLEXITY :- O(n)
+//using bfs and queue traversal converting the TreeNode to a string i.e Serialize 
+//and again converting the String data into the given TreeNode i.e Deserialization
+//here I am taking the null values as "#" and the comma "," as the spaces " "
+public class Codec {
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                res.append("n ");
+                continue;
+            }
+            res.append(node.val + " ");
+            q.add(node.left);
+            q.add(node.right);
+        }
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] values = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        q.add(root);
+        for (int i = 1; i < values.length; i++) {
+            TreeNode parent = q.poll();
+            if (!values[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                q.add(left);
+            }
+            if (!values[++i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                q.add(right);
+            }
+        }
+        return root;
+    }
+}
