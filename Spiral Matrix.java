e32c35e070e9237ac6d7170ebe7c1eb342d9027e
Sagnik Ghosal <122097465+sagnikghosal007@users.noreply.github.com>
2025-05-04 16:15:37 +0530

    Create Spiral Matrix.java

---
+++ Spiral Matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans=new ArrayList<>();
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=m-1;
+        while(top<=bottom && left<=right){
+            //top row;
+            for(int i=left;i<=right;i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            //right column
+            for(int i=top;i<=bottom;i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top<=bottom){
+                //bottom row
+                for(int i=right;i>=left;i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+
+    }
+}
